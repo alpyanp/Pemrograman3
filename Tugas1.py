9f8767ee4fb843c0644e55219552bfd40e835b34
alpyanp <94632535+alpyanp@users.noreply.github.com>
2021-11-19 00:23:52 +0700

    Tugas Pemograman 3
    Dengan disediakan menu maka user dapat memilih pilihan untuk menjalankan program yang akan di jalankan.
    
    Ket :
    1 = Ya
    0 = Tidak

--- Tugas1.py
+++ Tugas1.py
@@ -5,32 +5,57 @@ nama = "Alpyan Padjar Rizki"
 nim = "3220609"
 
 def menu():
+    print ("======== {} ========".format(nama))
     print("1. Untuk melihat kalender perbulan")
     print("2. Untuk melihat kalender pertahun")
     print("3. Untuk menentukan hari dari pencarian tanggal")
     print("4. Untuk menentukan tahun kabisat atau bukan")
-
+    opsi = input("Masukan Pilihan : ")
+    print ("============== {} ==============".format(nim))
+    if opsi == "1":
+        bulanan()
+    elif opsi == "2":
+        tahunan()
+    elif opsi == "3":
+        harian()
+    elif opsi == "4":
+        kabisat()
+    else :
+        print("Menu tidak ditemukan!")
 
 def bulanan():
     # Untuk melihat kalender perbulan
     tahun = int(input("Masukkan Tahun : "))
     bulan = int(input("Masukkan Bulan : "))
     print (" ") 
-    print ("======== {} ========".format(nama))
     print ("Kalender Pada Bulan ke {} Tahun {} :".format(bulan,tahun))
-    print ("============== {} ==============".format(nim))
     print (" ")
     print(calendar.month(tahun,bulan))
+    lanjut = input("Lanjut ? 1 / 0 : ")
+    if lanjut == "1":
+        menu()
+    else:
+        exit()
 
 def tahunan():
     # Untuk melihat kalender pertahun
     tahun1 = int (input ("Masukkan Tahun : "))
     print(calendar.calendar(tahun1))
+    lanjut = input("Lanjut ? 1 / 0 : ")
+    if lanjut == "1":
+        menu()
+    else:
+        exit()
     
 def kabisat():
     yy = int (input ("Masukkan Tahun : "))
     print ("Apakah Tahun {} Merupakan Tahun Kabisat : ".format(yy))
     print (calendar.isleap(yy))
+    lanjut = input("Lanjut ? 1 / 0 : ")
+    if lanjut == "1":
+        menu()
+    else:
+        exit()
     
 def harian():
     print ("Program Menentukan Hari")
@@ -54,18 +79,11 @@ def harian():
         print ("Hari Pada Tanggal {} Bulan {} Tahun {} Adalah Sabtu".format(tgl,bln,thn))
     if day_week_day == 6:
         print ("Hari Pada Tanggal {} Bulan {} Tahun {} Adalah Minggu".format(tgl,bln,thn))
-        
-print ("======== {} ========".format(nama))
+    lanjut = input("Lanjut ? 1 / 0 : ")
+    if lanjut == "1":
+        menu()
+    else:
+        exit()
+
 menu()
-opsi = input("Masukan Pilihan : ")
-if opsi == 1:
-    bulanan()
-elif opsi == 2:
-    tahunan()
-elif opsi == 3:
-    harian()
-elif opsi == 4:
-    kabisat()
-else :
-    print("Menu tidak ditemukan!")
-print ("============== {} ==============".format(nim))
+
